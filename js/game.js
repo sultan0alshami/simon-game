a7050ba2762c60667aa0f33564a2d2f48d1106bb
sultan alshami <53308736+sultan0alshami@users.noreply.github.com>
2022-05-22 19:19:50 +0300

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,84 @@
+
+var userClickedPattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var started = false;
+
+var level = 0;
+
+function nextSequence() {
+
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").html("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+}
+
+$(".btn").on("click", function handler(){
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(() => {
+        $("#" + currentColour).removeClass("pressed")
+    }, 100);
+}
+
+$(".button").on("click", () => {
+    if(!started){
+        $("#level-title").html("Level " + level);
+        nextSequence();
+        started = true;
+        $(".button").fadeOut();
+    }
+});
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] == userClickedPattern[currentLevel]){
+        if (gamePattern.length === userClickedPattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").html("Congratulations 🎉 You Reached To Level " + level + ", Press The Button To Restart");
+        $(".button").html("Restart").fadeIn();
+
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
